d2d502eb574d545387dccf153b8cd22d67114962
Gabriel Iciarte <gabriel_viki@hotmail.com>
2026-02-01 16:19:28 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,22 @@ import { RaceModule } from './race/race.module';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { Player } from './race/entities/race.entity';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-        TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',       // IP o host del servidor MySQL
-      port: 3306,
-      username: 'crypto_main',  // usuario MySQL
-      password: 'pastasc0',  // contraseña MySQL
-      database: 'crypto_race',     // nombre de la base de datos
-      entities: [User, Player],   // todas tus entidades aquí
-      synchronize: true,        // solo desarrollo; crea/actualiza tablas automáticamente
-    }),RaceModule, UserModule],
+    ConfigModule.forRoot({ isGlobal: true }), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Player],
+      synchronize: true,
+    }), RaceModule, UserModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
