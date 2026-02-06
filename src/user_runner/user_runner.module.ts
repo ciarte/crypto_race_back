ed4d89b438d00cf66ba3d1bafb550bc98d37d660
Gabriel Iciarte <gabriel_viki@hotmail.com>
2026-02-06 00:11:26 -0300

    fix

--- src/user_runner/user_runner.module.ts
+++ src/user_runner/user_runner.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRunnerService } from './user_runner.service';
 import { UserRunnerController } from './user_runner.controller';
+import { UserRunner } from './entities/user-runner.entity';
+import { Runner } from 'src/user/entities/runner.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserRunner, Runner]),
+  ],
   controllers: [UserRunnerController],
   providers: [UserRunnerService],
+  exports: [UserRunnerService],
 })
 export class UserRunnerModule {}
